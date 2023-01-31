b1a2278a249ba36422f6dd9927e0649c1f45f6d7
wanglonglong3 <wanglonglong3@gome.com.cn>
2023-01-31 18:44:02 +0800

    添加文件

--- Python-Crash-Course-Homework/chapter9/car.py
+++ Python-Crash-Course-Homework/chapter9/car.py
@@ -34,3 +34,39 @@ class Car:
     def increment_odometer(self, miles):
         """将里程表读数增加指定的量。"""
         self.odometer_reading += miles
+
+
+"""一组用于表示燃油汽车和电动汽车的类。"""
+
+
+class Battery:
+    """一次模拟电动汽车电瓶的简单尝试。"""
+
+    def __init__(self, battery_size=75):
+        """初始化电瓶的属性。"""
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """打印一条描述电瓶容量的消息。"""
+        print(f"This car has a {self.battery_size}-kWh battery.")
+
+    def get_range(self):
+        """打印一条描述电瓶续航里程的消息。"""
+        if self.battery_size == 75:
+            range = 260
+        elif self.battery_size == 100:
+            range = 315
+
+        print(f"This car can go about {range} miles on a full charge.")
+
+
+class ElectricCar(Car):
+    """模拟电动汽车的独特之处。"""
+
+    def __init__(self, make, model, year):
+        """
+        初始化父类的属性。
+        再初始化电动汽车特有的属性。
+        """
+        super().__init__(make, model, year)
+        self.battery = Battery()
