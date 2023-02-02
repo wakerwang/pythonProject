73addb49fa4f0cb93a8086e2fc73d689124406df
wanglonglong <804863893@qq.com>
2023-02-03 00:13:50 +0800

    第十章和随书源码

---
+++ Python-Crash-Course-Homework/chapter_10/pi_string.py
@@ -0,0 +1,15 @@
+# coding:utf-8
+
+with open('../../source_files/chapter_10/pi_million_digits.txt') as file_object:
+    lines = file_object.readlines()
+
+pi_string = ''
+for line in lines:
+    pi_string += line.strip()
+
+birthday = input("Enter your birthday, in the form mmddyy: ")
+if birthday in pi_string:
+    print("Your birthday appears in the first million digits of pi!")
+else:
+    print("Your birthday does not appear in the first million digits of pi.")
+
