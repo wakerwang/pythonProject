e5e27153f2147e8438947908ec91c25863149066
wanglonglong <804863893@qq.com>
2023-02-08 13:59:15 +0800

    第十章

--- Python-Crash-Course-Homework/chapter_10/remember_me.py
+++ Python-Crash-Course-Homework/chapter_10/remember_me.py
@@ -2,8 +2,16 @@
 
 import json
 
-username = input("What's your name?\n")
 filename = 'username.json'
-with open(filename, 'w') as f:
-    json.dump(username, f)
-    print(f"We'll remember you when you come back,{username}")
+try:
+    with open(filename) as f:
+        username = json.load(f)
+except FileNotFoundError:
+    username = input("What's your name?")
+    with open(filename, 'w') as f:
+        json.dump(username, f)
+        print(f"We'll remember you when you come back,{username}")
+
+else:
+    print(f"Welcome back,{username}")
+
